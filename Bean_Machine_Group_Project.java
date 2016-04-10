f62a47a4d3ae3e164b32f635222c582f5e0517f5
Miguel McCrary <mccrarym@student.swosu.edu>
2016-04-10 13:13:59 -0500

    adding java project to remote repository.

---
+++ Bean_Machine_Group_Project.java
@@ -0,0 +1,11 @@
+
+public class Bean_Machine_Group_Project {
+
+	public static void main(String[] args) {
+		// Outline the code for the Bean Machine and divide up work evenly.
+		
+		System.out.println("Hello World");
+
+	}
+
+}
