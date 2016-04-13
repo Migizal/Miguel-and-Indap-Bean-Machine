781708b62038ef8f2b861aa5de96e92bc2827fdb
Miguel McCrary <mccrarym@student.swosu.edu>
2016-04-12 21:46:49 -0500

    added array and some of the for loop.

--- Bean_Machine_Group_Project.java
+++ Bean_Machine_Group_Project.java
@@ -1,6 +1,21 @@
 
 public class Bean_Machine_Group_Project {
-
+	
+/*Method 3: Execute game: Miguel
+		 * Setup array for bins
+		 * setup for loop 
+		 * inside for loop call custom method to return which bin get a bean.
+		 */
+		public static int bins;
+		 public static void BeanMachine(){
+			 
+			int[] binarray = new int[bins + 1];
+		
+		for (int i = 0; i < bins; i++){
+			 
+		 }
+		
+		}
 	public static void main(String[] args) {
 		// Outline the code for the Bean Machine and divide up work evenly.
 		
@@ -21,11 +36,7 @@ public class Bean_Machine_Group_Project {
 		 * ask user for # of balls(limit # of balls)
 		 */
 		
-		/*Method 3: Execute game: Miguel
-		 * Setup array for bins
-		 * setup for loop 
-		 * inside for loop call custom method to return which bin get a bean.
-		 */
+		
 		
 		/*Method 3b: bean dropping: Miguel
 		 * return if it goes right or left.
