f59272caba713fba9c689756237d6c0daabab203
Miguel McCrary <mccrarym@student.swosu.edu>
2016-04-12 19:07:47 -0500

    Added a almost full outline of the program.

--- Bean_Machine_Group_Project.java
+++ Bean_Machine_Group_Project.java
@@ -4,7 +4,39 @@ public class Bean_Machine_Group_Project {
 	public static void main(String[] args) {
 		// Outline the code for the Bean Machine and divide up work evenly.
 		
-		System.out.println("Hello World");
+		//System.out.println("Hello World");
+		
+		//call the run method
+		
+		//call the test method
+		
+		/*Method 1: greet user:
+		 * write a description to greet user
+		 * explain the game
+		 * tell the user the options
+		 */
+		
+		/*Method 2: get user input:
+		 * ask user for # of bins
+		 * ask user for # of balls(limit # of balls)
+		 */
+		
+		/*Method 3: Execute game
+		 * Setup array for bins
+		 * setup for loop 
+		 * inside for loop call custom method to return which bin get a bean.
+		 */
+		
+		/*Method 3b: bean dropping:
+		 * return if it goes right or left.
+		 */
+		
+		//Method 4: Print out results
+		 
+		
+		//Method 5: Tell the user the game is over
+		System.out.println("Thank you for playing!");
+		 
 
 	}
 
